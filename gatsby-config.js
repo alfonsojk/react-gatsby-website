56ccd6ec4d1660db3252ceaef49f742b011b4df6
alfonsojk <alfonso.juarezkelly@gmail.com>
2021-02-24 10:06:30 -0600

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Travel Website`,
+    description: `Travel website showcasing the best travel destinations and deals online`,
+    author: `@alfonsojk`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
---
+++ src/data/MenuData.js
@@ -0,0 +1,7 @@
+export const MenuData = [
+    {title:"About Us", link:"/about"},
+    {title:"Trips", link:"/trips"},
+    {title:"Career", link:"/career"},
+    {title:"Contact Us", link:"/contact-us"},
+
+]
